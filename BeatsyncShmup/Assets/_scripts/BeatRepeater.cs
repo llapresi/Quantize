06ee928e6edfe8eb0202f317a1c4414f2a8de931
llapresi <lxl6996@rit.edu>
2017-04-12 23:21:36 -0400

    FIrst commit

---
+++ BeatsyncShmup/Assets/_scripts/BeatRepeater.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Initial test of beat synchonization, using only time
+/// </summary>
+public class BeatRepeater : MonoBehaviour {
+
+    public float bpm;
+    public AudioClip song;
+    AudioSource source;
+    float nextBeat;
+    float samplesPerBeat;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        source.clip = song;
+        source.Play();
+
+        samplesPerBeat = song.frequency / (bpm / 60);
+        CalculateBeat();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(source.timeSamples >= nextBeat)
+        {
+            //Debug.Log("beat");
+            //CalculateBeat();
+        }
+
+        Debug.Log("dspTime: " + (float)(AudioSettings.dspTime));
+        Debug.Log("TS: " + source.timeSamples);
+    }
+
+    void CalculateBeat()
+    {
+        nextBeat = source.timeSamples + samplesPerBeat;
+    }
+}
